d5937d1d76fd46286ba81b49e3a123abfb73ad4d
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-11-08 23:39:33 -0500

    INGRESO DEL PROYECTO

---
+++ Flexguaraje Peru/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <div>
+        {/* ALEXANDERRRRR */}
+      </div>
+      <div>
+        {/* CESARRRRRRR */}
+      </div>
+      <div>
+        {/* LOHANAAAAAAAA */}
+      </div>
+      <div>
+        {/* CAMILAAAAAAAAA */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
