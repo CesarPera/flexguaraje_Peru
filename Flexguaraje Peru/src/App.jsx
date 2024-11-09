484cc33d773c6a03acdfa99e357fda7c58cdc464
alexanderfip <xanaxoliver@gmail.com>
2024-11-09 00:14:36 -0500

    we
    sdsdwe

--- Flexguaraje Peru/src/App.jsx
+++ Flexguaraje Peru/src/App.jsx
@@ -1,19 +1,50 @@
+import React from 'react';
+import './index.css';
+
 function App() {
   return (
-    <div>
-      <div>
-        {/* ALEXANDERRRRR */}
-      </div>
-      <div>
-        {/* CESARRRRRRR */}
-      </div>
-      <div>
-        {/* LOHANAAAAAAAA */}
-      </div>
-      <div>
-        {/* CAMILAAAAAAAAA */}
-      </div>
+    <div className="App">
+      <nav>
+        <div className="nav-container">
+          <div className="nav-left">
+            <h1>Garage</h1>
+          </div>
+          <div className="nav-right">
+            <ul>
+              <li><a href="#informacion">Información</a></li>
+              <li><a href="#texto">Texto</a></li>
+              <li><a href="#contacto">Contacto</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <section id="informacion">
+        <h2>Información</h2>
+        <p>Aquí va la información sobre el garage.</p>
+      </section>
+
+      <section id="texto">
+        <h2>Texto</h2>
+        <p>Contenido de texto relacionado con el garage.</p>
+      </section>
+
+      <section id="contacto">
+        <h2>Contacto</h2>
+        <p>Información de contacto del garageeeee.</p>
+      </section>
     </div>
+
+
+
+
+
+
+
+
+
+
+
   );
 }
 
