84050c4b4e032696648137c662bb9bd0722d336b
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-11-10 18:26:49 -0500

    modificaciones de la web 1

--- Flexguaraje Peru/src/App.jsx
+++ Flexguaraje Peru/src/App.jsx
@@ -4,10 +4,11 @@ import './index.css';
 function App() {
   return (
     <div className="App">
+      {/* ALEXANDERRRRRRR */}
       <nav>
         <div className="nav-container">
           <div className="nav-left">
-            <h1>Garage</h1>
+            <h1>Flexguaraje Peru</h1>
           </div>
           <div className="nav-right">
             <ul>
@@ -19,15 +20,20 @@ function App() {
         </div>
       </nav>
 
+      {/* CESARRRRRRRRRRRRRR */}
       <section id="informacion">
         <h2>Información</h2>
         <p>Aquí va la información sobre el garage.</p>
       </section>
 
+      {/* LOHANAAAAAAAAAAA */}
+
       <section id="texto">
         <h2>Texto</h2>
         <p>Contenido de texto relacionado con el garage.</p>
       </section>
+
+      {/* CAMILAAAAAAAAAAAAAAA */}
 
       <section id="contacto">
         <h2>Contacto</h2>
