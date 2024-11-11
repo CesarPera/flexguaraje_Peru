88a46f62d2c07c899f5e3821652e58960e429e23
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-11-10 19:57:40 -0500

    AVANCE 1 CESAR

--- Flexguaraje Peru/src/App.jsx
+++ Flexguaraje Peru/src/App.jsx
@@ -22,8 +22,45 @@ function App() {
 
       {/* CESARRRRRRRRRRRRRR */}
       <section id="informacion">
-        <h2>Información</h2>
-        <p>Aquí va la información sobre el garage.</p>
+        <div className='quienes-somos'>
+          <div className='informacion-l'>
+            <h2>¿Quienes somos?</h2>
+            <p>FlexGuaraje Perú es una empresa dedicada al alquiler de espacios de almacenamiento para personas y empresas. Nuestro objetivo es brindar soluciones prácticas y seguras para el resguardo de bienes, adaptándonos a las necesidades de cada cliente. Nos especializamos en ofrecer mini-almacenes tipo garaje con una ubicación conveniente en la ciudad de Lima.</p>
+          </div>
+          <div className='informacion-i'>
+            <img src="https://s3.eu-west-1.amazonaws.com/bucketeer-e01fe271-7b8b-4ee5-b0b3-cc60bc326895/blog/is-self-storage-commercial-or-industrial/cover/facility-indoor-green.jpg" alt="img-garaje" />
+          </div>
+        </div>
+
+        <div className='objetivo'>
+          <div className='informacion-i'>
+            <img src="https://concepto.de/wp-content/uploads/2019/12/objetivo-general-e1576544846604.jpg" alt="objetivo-empresa" />
+          </div>
+          <div className='informacion-l'>
+            <h2>Objetivo</h2>
+            <p>Ofrecer espacios de almacenamiento confiables y accesibles, que permitan a nuestros clientes tener una solución segura y conveniente para el resguardo de sus bienes.</p>
+          </div>
+        </div>
+
+        <div className='mision'>
+          <div className='informacion-l'>
+            <h2>Misión</h2>
+            <p>Brindar un servicio de almacenamiento de alta calidad, caracterizado por la seguridad, accesibilidad y atención personalizada. Nos esforzamos por ser la opción preferida de empresas y personas que buscan un espacio seguro y práctico para almacenar sus pertenencias.</p>
+          </div>
+          <div className='informacion-i'>
+            <img src="https://img.freepik.com/foto-gratis/retrato-guardia-seguridad-masculino-estacion-radio-pantallas-camara_23-2150368714.jpg" alt="mision-empresa" />
+          </div>
+        </div>
+
+        <div className='vision'>
+          <div className='informacion-i'>
+            <img src="https://media.istockphoto.com/id/1393131806/es/vector/trofeo-al-%C3%A9xito-empresarial-los-ganadores-de-negocios-suben-las-escaleras-hacia-el-cielo.jpg?s=612x612&w=0&k=20&c=IZIwp1cdUVb880BDL3RuNhPt-4rBN9X4O4aKY9EYnrc=" alt="vision-empresa" />
+          </div>
+          <div className='informacion-l'>
+            <h2>Visión</h2>
+            <p>Ser reconocidos como líderes en el sector de almacenamiento en Lima, destacándonos por nuestra innovación, calidad de servicio y compromiso con la satisfacción de nuestros clientes, fomentando una relación de confianza y tranquilidad.</p>
+          </div>
+        </div>
       </section>
 
       {/* LOHANAAAAAAAAAAA */}
