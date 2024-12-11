91b738e8c4a3a582ee7d1fea0626048b4c4cca3f
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-12-10 22:12:08 -0500

    para lohana

--- Admin-Flexguaraje/Admin-Flexguaraje-Front-end/src/paginas/Clientes/Clientes.jsx
+++ Admin-Flexguaraje/Admin-Flexguaraje-Front-end/src/paginas/Clientes/Clientes.jsx
@@ -150,101 +150,52 @@ function Clientes() {
             {mostrarFormulario && (
                 <div className="formulario-flotante">
                     <div className="formulario-contenido">
-                        <h3>Añadir Nuevo Cliente</h3>
+                        <h3 className='text-center'>Añadir Nuevo Cliente:</h3>
                         <div className="formulario-campos"> {/* Contenedor con desplazamiento vertical */}
-                            <label>Tipo de Cliente</label>
-                            <select
-                                value={nuevoCliente.tipo}
+                            <label>Nombres:</label>
+                            <input
+                                type="text"
+                                value={nuevoCliente.nombre}
                                 onChange={(e) =>
-                                    setNuevoCliente({ ...nuevoCliente, tipo: e.target.value })
+                                    setNuevoCliente({ ...nuevoCliente, nombre: e.target.value })
                                 }
-                            >
-                                <option value="Empresa">Empresa</option>
-                                <option value="Persona">Persona</option>
-                            </select>
-
-                            {nuevoCliente.tipo === "Persona" ? (
-                                <>
-                                    <label>Nombre</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.nombre}
-                                        onChange={(e) =>
-                                            setNuevoCliente({ ...nuevoCliente, nombre: e.target.value })
-                                        }
-                                    />
-                                    <label>Apellido</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.apellido}
-                                        onChange={(e) =>
-                                            setNuevoCliente({
-                                                ...nuevoCliente,
-                                                apellido: e.target.value,
-                                            })
-                                        }
-                                    />
-                                    <label>DNI</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.dni}
-                                        onChange={(e) =>
-                                            setNuevoCliente({ ...nuevoCliente, dni: e.target.value })
-                                        }
-                                    />
-                                    <label>Edad</label>
-                                    <input
-                                        type="number"
-                                        value={nuevoCliente.edad}
-                                        onChange={(e) =>
-                                            setNuevoCliente({ ...nuevoCliente, edad: e.target.value })
-                                        }
-                                    />
-                                    <label>Fecha de Nacimiento</label>
-                                    <input
-                                        type="date"
-                                        value={nuevoCliente.fechaNacimiento}
-                                        onChange={(e) =>
-                                            setNuevoCliente({ ...nuevoCliente, fechaNacimiento: e.target.value })
-                                        }
-                                    />
-                                </>
-                            ) : (
-                                <>
-                                    <label>Nombre de la Empresa</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.nombreEmpresa}
-                                        onChange={(e) =>
-                                            setNuevoCliente({
-                                                ...nuevoCliente,
-                                                nombreEmpresa: e.target.value,
-                                            })
-                                        }
-                                    />
-                                    <label>RUC</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.ruc}
-                                        onChange={(e) =>
-                                            setNuevoCliente({ ...nuevoCliente, ruc: e.target.value })
-                                        }
-                                    />
-                                    <label>Dirección</label>
-                                    <input
-                                        type="text"
-                                        value={nuevoCliente.direccion}
-                                        onChange={(e) =>
-                                            setNuevoCliente({
-                                                ...nuevoCliente,
-                                                direccion: e.target.value,
-                                            })
-                                        }
-                                    />
-                                </>
-                            )}
-
-                            <label>Correo</label>
+                            />
+                            <label>Apellidos:</label>
+                            <input
+                                type="text"
+                                value={nuevoCliente.apellido}
+                                onChange={(e) =>
+                                    setNuevoCliente({
+                                        ...nuevoCliente,
+                                        apellido: e.target.value,
+                                    })
+                                }
+                            />
+                            <label>Dni:</label>
+                            <input
+                                type="text"
+                                value={nuevoCliente.dni}
+                                onChange={(e) =>
+                                    setNuevoCliente({ ...nuevoCliente, dni: e.target.value })
+                                }
+                            />
+                            <label>Fecha de Nacimiento:</label>
+                            <input
+                                type="date"
+                                value={nuevoCliente.fechaNacimiento}
+                                onChange={(e) =>
+                                    setNuevoCliente({ ...nuevoCliente, fechaNacimiento: e.target.value })
+                                }
+                            />
+                            <label>Edad:</label>
+                            <input
+                                type="number"
+                                value={nuevoCliente.edad}
+                                onChange={(e) =>
+                                    setNuevoCliente({ ...nuevoCliente, edad: e.target.value })
+                                }
+                            />
+                            <label>Correo Electronico:</label>
                             <input
                                 type="email"
                                 value={nuevoCliente.correo}
@@ -252,7 +203,7 @@ function Clientes() {
                                     setNuevoCliente({ ...nuevoCliente, correo: e.target.value })
                                 }
                             />
-                            <label>Teléfono</label>
+                            <label>Teléfono:</label>
                             <input
                                 type="text"
                                 value={nuevoCliente.telefono}
@@ -260,21 +211,12 @@ function Clientes() {
                                     setNuevoCliente({ ...nuevoCliente, telefono: e.target.value })
                                 }
                             />
-                            <label>Provincia</label>
+                            <label>Dirección:</label>
                             <input
                                 type="text"
                                 value={nuevoCliente.provincia}
                                 onChange={(e) =>
                                     setNuevoCliente({ ...nuevoCliente, provincia: e.target.value })
-                                }
-                            />
-
-                            <label>Fecha de Inicio</label>
-                            <input
-                                type="date"
-                                value={nuevoCliente.fechaInicio}
-                                onChange={(e) =>
-                                    setNuevoCliente({ ...nuevoCliente, fechaInicio: e.target.value })
                                 }
                             />
                         </div>
