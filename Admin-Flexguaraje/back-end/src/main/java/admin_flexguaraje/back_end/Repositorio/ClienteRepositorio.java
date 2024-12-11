67a5d8d5c363ae1bedc2a88ae5e34289af948ed4
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-12-10 22:19:20 -0500

    CORREGIDO PROBLEMA

--- Admin-Flexguaraje/back-end/src/main/java/admin_flexguaraje/back_end/Repositorio/ClienteRepositorio.java
+++ Admin-Flexguaraje/back-end/src/main/java/admin_flexguaraje/back_end/Repositorio/ClienteRepositorio.java
@@ -2,15 +2,7 @@ package admin_flexguaraje.back_end.Repositorio;
 
 import admin_flexguaraje.back_end.Modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< Updated upstream
 
 public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
     public Cliente findByDni(String dni);
-
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
->>>>>>> Stashed changes
 }
