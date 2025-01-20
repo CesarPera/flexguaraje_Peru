9c8e9362eb6ea1669777f0c4aaba641fa26d671f
Cesar Carhuas <cesarcarhuas5@gmail.com>
2025-01-20 01:17:58 -0500

    LA BASE DE DATOS MODIFICADO

--- Admin-Flexguaraje/BD_FLEXGUARAJE_PERU.sql
+++ Admin-Flexguaraje/BD_FLEXGUARAJE_PERU.sql
@@ -8,8 +8,11 @@ create table roles (
     estado varchar(15) not null default 'Activo',
     constraint UQ_nombre_rol UNIQUE (nombre_rol),
 	CONSTRAINT valores_estado_roles CHECK (estado IN ('Activo', 'Desactivado'))
-
 );
+# DATOS PARA LA TABLA ROLES
+INSERT INTO roles (nombre_rol, estado) VALUES 
+('Administrador', 'Activo'),
+('Propietario', 'Activo');
 
 # TABLAAAAAAAA PERMISOSSSSSSSSSS
 create table permisos (
@@ -21,46 +24,73 @@ create table permisos (
 	CONSTRAINT valores_Estado_permisos CHECK (estado IN ('Activo', 'Desactivado')),
 	constraint nombre_permiso UNIQUE (nombre_permiso)
 );
+# DATOS PARA LA TABLA PERMISOS.
+INSERT INTO permisos (id_roles, nombre_permiso, estado) VALUES 
+(1, 'Gestión', 'Activo'),
+(2, 'Supervisión', 'Activo');
 
-
-# TABLAAAAA CUENTAAAAAAAAAAAAAA
-create table cuenta (
-	id_cuenta int primary key auto_increment,
-    id_roles int not null,
-    usuario varchar(20) not null,
-    email varchar(50) not null,
-    pass varchar(30) not null,
-    estado varchar(15) not null default 'Activo',
-    constraint FK_roles_cuenta foreign key (id_roles) references roles(id_roles),
-	CONSTRAINT valores_estado_cuenta CHECK (estado IN ('Activo', 'Desactivado')),
-    constraint UQ_email_cuenta UNIQUE (email),
-    constraint UQ_usuario_cuenta UNIQUE (usuario)
-);
-
-# TABLA ADMINISTRADORRRRRRRRRR
-create table administrador (
-	id_admin int primary key auto_increment,
-    id_cuenta int not null,
+# TABLA usuario ( administrador, propietario y otros)
+create table usuario (
+	id_usuario int primary key auto_increment,
     dni varchar(8) not null,
     nombre varchar(30) not null,
     apellido_paterno varchar(20) not null,
     apellido_materno varchar(20) not null,
     email varchar(30) not null,
     telefono varchar(9) not null,
-	constraint FK_admin_cuenta foreign key (id_cuenta) references cuenta(id_cuenta)
+	CONSTRAINT UQ_dni UNIQUE (dni)
 );
+# 15 DATOS PARA LA TABLA USUARIOS
+INSERT INTO usuario (dni, nombre, apellido_paterno, apellido_materno, email, telefono) VALUES
+('12345678', 'Juan', 'Pérez', 'López', 'juan.perez@example.com', '912345678'),
+('23456789', 'María', 'Gómez', 'Sánchez', 'maria.gomez@example.com', '923456789'),
+('34567890', 'Carlos', 'Ramírez', 'Martínez', 'carlos.ramirez@example.com', '934567890'),
+('45678901', 'Ana', 'Torres', 'Vargas', 'ana.torres@example.com', '945678901'),
+('56789012', 'Luis', 'Fernández', 'Rojas', 'luis.fernandez@example.com', '956789012'),
+('67890123', 'Lucía', 'Díaz', 'Hernández', 'lucia.diaz@example.com', '967890123'),
+('78901234', 'Pedro', 'Castillo', 'Morales', 'pedro.castillo@example.com', '978901234'),
+('89012345', 'Elena', 'Ramos', 'Guzmán', 'elena.ramos@example.com', '989012345'),
+('90123456', 'Jorge', 'Alvarez', 'Paredes', 'jorge.alvarez@example.com', '900123456'),
+('01234567', 'Camila', 'Ruiz', 'Salas', 'camila.ruiz@example.com', '911234567'),
+('11234567', 'Sofía', 'Mendoza', 'Chávez', 'sofia.mendoza@example.com', '922345678'),
+('22345678', 'Miguel', 'Vega', 'Ortega', 'miguel.vega@example.com', '933456789'),
+('33456789', 'Isabel', 'Cruz', 'Flores', 'isabel.cruz@example.com', '944567890'),
+('44567890', 'Ricardo', 'Lopez', 'Rios', 'ricardo.lopez@example.com', '955678901'),
+('55678901', 'Daniela', 'Ponce', 'Espinoza', 'daniela.ponce@example.com', '966789012');
 
-# TABLA PROPIETARIOOOOOOOOOO
-create table propietario (
-	id_pripietario int primary key auto_increment,
-    id_cuenta int not null,
-	dni varchar(8) not null,
-    nombre varchar(30) not null,
-    apellido_paterno varchar(20) not null,
-    apellido_materno varchar(20) not null,
-	telefono varchar(9) not null,
-	constraint FK_propit_cuenta foreign key (id_cuenta) references cuenta(id_cuenta)
- );
+# TABLAAAAA CUENTAAAAAAAAAAAAAA
+create table cuenta (
+	id_cuenta int primary key auto_increment,
+    id_usuario int not null,
+    id_roles int not null,
+    usuario varchar(20) not null,
+    email varchar(50) not null,
+    pass varchar(30) not null,
+    estado varchar(15) not null default 'Activo',
+    constraint FK_roles_cuenta foreign key (id_roles) references roles(id_roles),
+	constraint FK_usuario_cuenta foreign key (id_usuario) references usuario(id_usuario),
+	CONSTRAINT valores_estado_cuenta CHECK (estado IN ('Activo', 'Desactivado')),
+    constraint UQ_email_cuenta UNIQUE (email),
+    constraint UQ_usuario_cuenta UNIQUE (usuario)
+);
+# 15 DATOS PARA LA TABLA CUENTA
+INSERT INTO cuenta (id_usuario, id_roles, usuario, email, pass, estado)
+VALUES
+(1, 2, 'propietario1', 'propietario1@example.com', 'password123', 'Activo'),
+(2, 1, 'admin1', 'admin1@example.com', 'password123', 'Activo'),
+(3, 1, 'admin2', 'admin2@example.com', 'password123', 'Activo'),
+(4, 1, 'admin3', 'admin3@example.com', 'password123', 'Activo'),
+(5, 1, 'admin4', 'admin4@example.com', 'password123', 'Activo'),
+(6, 1, 'admin5', 'admin5@example.com', 'password123', 'Activo'),
+(7, 1, 'admin6', 'admin6@example.com', 'password123', 'Desactivado'),
+(8, 1, 'admin7', 'admin7@example.com', 'password123', 'Desactivado'),
+(9, 1, 'admin8', 'admin8@example.com', 'password123', 'Desactivado'),
+(10, 1, 'admin9', 'admin9@example.com', 'password123', 'Desactivado'),
+(11, 1, 'admin10', 'admin10@example.com', 'password123', 'Desactivado'),
+(12, 1, 'admin11', 'admin11@example.com', 'password123', 'Desactivado'),
+(13, 1, 'admin12', 'admin12@example.com', 'password123', 'Desactivado'),
+(14, 1, 'admin13', 'admin13@example.com', 'password123', 'Desactivado'),
+(15, 1, 'admin14', 'admin14@example.com', 'password123', 'Desactivado');
 
 #TABLA CLIENTESSSSSSSSSSSS
 CREATE TABLE cliente (
@@ -82,6 +112,27 @@ CREATE TABLE cliente (
                                                  apellido_paterno NOT LIKE '%[^A-Za-z]%' AND 
                                                  apellido_materno NOT LIKE '%[^A-Za-z]%')
 );
+# 20 DATOS DE LA TABLA CLIENTE
+INSERT INTO cliente (id_cuenta, dni, nombre, apellido_paterno, apellido_materno, telefono, email, direccion, nota_adicional) VALUES
+(2, '12345678', 'Carlos', 'Gomez', 'Lopez', '912345678', 'carlos.gomez@example.com', 'Av. Siempre Viva 123, Lima', 'Sin Discapacidad'),
+(3, '23456789', 'Maria', 'Torres', 'Perez', '923456789', 'maria.torres@example.com', 'Jr. Los Pinos 456, Arequipa', 'Sin Discapacidad'),
+(4, '34567890', 'Juan', 'Ramirez', 'Garcia', '934567890', 'juan.ramirez@example.com', 'Calle Los Olivos 789, Cusco', 'Sin Discapacidad'),
+(5, '45678901', 'Elena', 'Martinez', 'Lopez', '945678901', 'elena.martinez@example.com', 'Av. Las Flores 321, Trujillo', 'Usuario con silla de ruedas'),
+(6, '56789012', 'Luis', 'Fernandez', 'Ramos', '956789012', 'luis.fernandez@example.com', 'Jr. San Juan 654, Ica', 'Sin Discapacidad'),
+(7, '67890123', 'Lucia', 'Diaz', 'Guzman', '967890123', 'lucia.diaz@example.com', 'Calle Principal 987, Piura', 'Usuario con bastón'),
+(8, '78901234', 'Pedro', 'Castillo', 'Alvarez', '978901234', 'pedro.castillo@example.com', 'Av. La Marina 147, Tacna', 'Sin Discapacidad'),
+(9, '89012345', 'Ana', 'Rios', 'Morales', '989012345', 'ana.rios@example.com', 'Jr. Amazonas 258, Chiclayo', 'Sin Discapacidad'),
+(10, '90123456', 'Jorge', 'Paredes', 'Flores', '900123456', 'jorge.paredes@example.com', 'Calle El Sol 369, Huancayo', 'Sin Discapacidad'),
+(11, '01234567', 'Sofia', 'Mendoza', 'Chavez', '911234567', 'sofia.mendoza@example.com', 'Jr. Las Palmeras 111, Puno', 'Sin Discapacidad'),
+(12, '11234567', 'Ricardo', 'Lopez', 'Salazar', '922345678', 'ricardo.lopez@example.com', 'Av. La Cultura 222, Juliaca', 'Usuario con discapacidad auditiva'),
+(13, '22345678', 'Daniela', 'Vega', 'Rojas', '933456789', 'daniela.vega@example.com', 'Calle Los Cedros 333, Tumbes', 'Sin Discapacidad'),
+(14, '33456789', 'Miguel', 'Cruz', 'Hernandez', '944567890', 'miguel.cruz@example.com', 'Jr. Los Claveles 444, Moquegua', 'Usuario con discapacidad visual'),
+(15, '44567890', 'Isabel', 'Ruiz', 'Espinoza', '955678901', 'isabel.ruiz@example.com', 'Av. San Martín 555, Chimbote', 'Sin Discapacidad'),
+(15, '55678901', 'Camila', 'Ponce', 'Vargas', '966789012', 'camila.ponce@example.com', 'Jr. Los Tulipanes 666, Cajamarca', 'Sin Discapacidad'),
+(11, '66789012', 'Jose', 'Alvarez', 'Paredes', '977890123', 'jose.alvarez@example.com', 'Calle Principal 777, Huánuco', 'Sin Discapacidad'),
+(9, '77890123', 'Karina', 'Chavez', 'Garcia', '988901234', 'karina.chavez@example.com', 'Jr. Los Jazmines 888, Ayacucho', 'Usuario con silla de ruedas'),
+(9, '88901234', 'Hector', 'Morales', 'Diaz', '999012345', 'hector.morales@example.com', 'Av. El Progreso 999, Cajamarca', 'Sin Discapacidad'),
+(4, '99012345', 'Paola', 'Garcia', 'Sanchez', '910123456', 'paola.garcia@example.com', 'Jr. Las Margaritas 123, Iquitos', 'Sin Discapacidad');
 
 #TABLA ESPACIOSSSSS
 CREATE TABLE espacio (
@@ -94,8 +145,7 @@ CREATE TABLE espacio (
     CONSTRAINT valores_estado CHECK (estado IN ('Disponible', 'Ocupado','Mantenimiento')),
     CONSTRAINT valores_subestado CHECK (subestado IN ('Activo', 'Desactivado'))
 );
-
-#TABLA ESPACIOS - DATOSSSSSSSSSSSSSS
+# 20 DATOSSSSSSSSSSSSSS DE LA TABLA ESPACIO
 INSERT INTO espacio VALUES
 (1, 'E001', 'Disponible', 'Desactivado',150),
 (2, 'E002', 'Disponible', 'Desactivado',150),
@@ -155,17 +205,80 @@ CREATE TABLE boleta (
 
 
 
+
 # ---------- CONSULTASSSSSSSSSS -----------
 use flexguaraje_peru;
+select * from roles;
+select * from permisos;
+select * from usuario;
+select * from cuenta;
 select * from cliente;
 select * from espacio;
 select * from alquileres;
 select * from boleta;
 
-DELETE FROM alquileres WHERE id_alquiler = 12;
+# VISUALIZAR LOS DATOS COMBINADOS DE ROLES Y PERMISOS
+SELECT 
+    r.nombre_rol, 
+    r.estado AS estado_rol, 
+    p.nombre_permiso, 
+    p.estado AS estado_permiso
+FROM roles r
+JOIN permisos p ON r.id_roles = p.id_roles;
 
+# VISUALIZAR LOS DATOS COMBINADOS DE USUARIO Y CUENTA
+SELECT 
+	u.dni,
+    u.nombre AS nombre_usuario,
+    u.apellido_paterno, 
+    u.apellido_materno, 
+    c.usuario AS cuenta_usuario, 
+    c.email AS cuenta_email,
+    c.pass AS cuenta_contraseña,
+    c.estado AS estado_cuenta
+FROM usuario u
+JOIN cuenta c ON u.id_usuario = c.id_usuario;
 
-# VISUALIZAR LOS DATOS DE LA TABLA COMBINADO - ALQUILER
+# VISUALIZAR LOS DATOS COMBINADOS DE ROLES, PERMISOS Y CUENTA
+SELECT 
+    r.nombre_rol, 
+    p.nombre_permiso, 
+    c.usuario AS cuenta_usuario, 
+    c.email AS cuenta_email,
+	c.estado AS estado_cuenta
+FROM roles r
+JOIN permisos p ON r.id_roles = p.id_roles
+JOIN cuenta c ON r.id_roles = c.id_roles;
+
+# VISUALIZAR LOS DATOS COMBINADOS DE CUENTA Y CLIENTE
+SELECT 
+    c.usuario AS cuenta_usuario, 
+    c.email AS cuenta_email, 
+    cl.dni AS cliente_dni, 
+    cl.nombre AS cliente_nombre, 
+    cl.apellido_paterno AS cliente_apellido_paterno,
+    cl.apellido_materno AS cliente_apellido_materno,
+    cl.telefono AS cliente_telefono, 
+    cl.direccion AS cliente_direccion, 
+    cl.nota_adicional AS cliente_nota_adicional
+FROM cuenta c
+JOIN cliente cl ON c.id_cuenta = cl.id_cuenta;
+
+# VISUALIZAR LOS DATOS COMBINADOS DE ALQUILERES, CLIENTE Y ESPACIO
+SELECT 
+	c.dni AS cliente_dni, 
+    CONCAT(c.nombre, ' ', c.apellido_paterno,' ', c.apellido_materno) AS nombre_completo,
+    e.codigo_espacio AS espacio_codigo,
+	e.estado AS estado_espacio,
+    e.costo AS espacio_costo,
+    a.fecha_inicio_alquiler, 
+    a.fecha_fin_alquiler, 
+    a.total_dias_alquiler
+FROM alquileres a
+JOIN cliente c ON a.id_cliente = c.id_cliente
+JOIN espacio e ON a.id_espacio = e.id_espacio;
+
+# VISUALIZAR LOS DATOS DE LA TABLA COMBINADO - ALQUILER CON ESPACIO AL MISMO TIEMPO
 SELECT 
     e.codigo_espacio AS codigoEspacio,
     e.estado AS estado,
@@ -182,18 +295,18 @@ LEFT JOIN
 LEFT JOIN 
     cliente c ON a.id_cliente = c.id_cliente;
 
-    
-
-# VISUALIZAR LOS DATOS DE LA TABLA COMBINADO - BOLETA
+# VISUALIZAR DATOS COMBINADAS DE BOLETA, ALQUILERES Y CUENTA
 SELECT 
     b.codigo_boleta, 
     b.fecha_emision, 
     b.metodo_pago, 
     b.monto_pagar, 
-    c.dni AS cliente_dni, 
-    e.codigo_espacio AS codigoEspacio
+    a.fecha_inicio_alquiler, 
+    a.fecha_fin_alquiler, 
+    c.usuario AS cuenta_usuario
 FROM boleta b
 JOIN alquileres a ON b.id_alquiler = a.id_alquiler
-JOIN cliente c ON a.id_cliente = c.id_cliente
-JOIN espacio e ON a.id_espacio = e.id_espacio;
+JOIN cuenta c ON b.id_cuenta = c.id_cuenta;
+
+
 
