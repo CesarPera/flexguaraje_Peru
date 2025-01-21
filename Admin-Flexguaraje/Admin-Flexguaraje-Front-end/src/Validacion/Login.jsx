6560d558a715ce367bbbea27d3a5146300ce69c9
Cesar Carhuas <cesarcarhuas5@gmail.com>
2025-01-21 00:32:01 -0500

    LOGIN INGRESO

--- Admin-Flexguaraje/Admin-Flexguaraje-Front-end/src/Validacion/Login.jsx
+++ Admin-Flexguaraje/Admin-Flexguaraje-Front-end/src/Validacion/Login.jsx
@@ -27,80 +27,83 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      {!isChangingPassword ? (
-        <form className="login-form" onSubmit={handleSubmit}>
-          <h2 className="form-title">{isRegistering ? 'Crear Cuenta' : 'Iniciar Sesión'}</h2>
-          <div className="input-group">
-            <label htmlFor="email" className="animated-label">Correo Electrónico</label>
-            <input
-              type="email"
-              id="email"
-              className="animated-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              placeholder=" "
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password" className="animated-label">Contraseña</label>
-            <input
-              type="password"
-              id="password"
-              className="animated-input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              placeholder=" "
-            />
-          </div>
-          <button type="submit" className="animated-button">
-            {isRegistering ? 'Crear Cuenta' : 'Iniciar Sesión'}
-          </button>
-          <p className="toggle-text" onClick={handlePasswordChangeMode}>
-            {isRegistering ? '' : 'Crear Cuenta'}
-          </p>
-        </form>
-      ) : (
-        <form className="login-form" onSubmit={handleSubmit}>
-          <h2 className="form-title"></h2>
-          <div className="input-group">
-            <label htmlFor="email" className="animated-label">Nombre de usuario</label>
-            <input
-              type="email"
-              id="email"
-              className="animated-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              placeholder=" "
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="oldPassword" className="animated-label">Correo electronico</label>
-            <input
-              type="password"
-              id="oldPassword"
-              className="animated-input"
-              required
-              placeholder=" "
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="newPassword" className="animated-label">Nueva Contraseña</label>
-            <input
-              type="password"
-              id="newPassword"
-              className="animated-input"
-              required
-              placeholder=" "
-            />
-          </div>
-          <button type="submit" className="animated-button">Crear Cuenta Nueva</button>
-        </form>
-      )}
+    <div className='general-login'>
+      <div className="login-container">
+        {!isChangingPassword ? (
+          <form className="login-form" onSubmit={handleSubmit}>
+            <h2 className="form-title">{isRegistering ? 'Crear Cuenta' : 'Iniciar Sesión'}</h2>
+            <div className="input-group">
+              <label htmlFor="email" className="animated-label">Correo Electrónico</label>
+              <input
+                type="email"
+                id="email"
+                className="animated-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder=" "
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password" className="animated-label">Contraseña</label>
+              <input
+                type="password"
+                id="password"
+                className="animated-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder=" "
+              />
+            </div>
+            <button type="submit" className="animated-button">
+              {isRegistering ? 'Crear Cuenta' : 'Iniciar Sesión'}
+            </button>
+            <p className="toggle-text" onClick={handlePasswordChangeMode}>
+              {isRegistering ? '' : 'Crear Cuenta'}
+            </p>
+          </form>
+        ) : (
+          <form className="login-form" onSubmit={handleSubmit}>
+            <h2 className="form-title"></h2>
+            <div className="input-group">
+              <label htmlFor="email" className="animated-label">Nombre de usuario</label>
+              <input
+                type="email"
+                id="email"
+                className="animated-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder=" "
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="oldPassword" className="animated-label">Correo electronico</label>
+              <input
+                type="password"
+                id="oldPassword"
+                className="animated-input"
+                required
+                placeholder=" "
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="newPassword" className="animated-label">Nueva Contraseña</label>
+              <input
+                type="password"
+                id="newPassword"
+                className="animated-input"
+                required
+                placeholder=" "
+              />
+            </div>
+            <button type="submit" className="animated-button">Crear Cuenta Nueva</button>
+          </form>
+        )}
+      </div>
     </div>
+
   );
 };
 
